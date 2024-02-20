22282ed323a0af3810908f1b99b19f023371cec7
K L Shashin Kalpajith <shashikalpajith6@gmail.com>
2024-02-20 17:01:57 +0530

    Install and Set Up Mongoose

--- myApp.js
+++ myApp.js
@@ -1,6 +1,12 @@
+let mongoose = require("mongoose")
 require('dotenv').config();
 
+mongoose.connect(process.env.MONGO_URI, 
+  { useNewUrlParser: true, 
+    useUnifiedTopology: true 
+  });
 
+  
 let Person;
 
 const createAndSavePerson = (done) => {
